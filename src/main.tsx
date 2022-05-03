718e63c14ba7906994071f3650bed028d3e0f930
Саша Кошарюк <a.koshariuk@dao-group.org>
2022-05-03 17:24:10 +0300

    ⚰️ removed strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -9,9 +9,7 @@ import { store } from './store/store';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
