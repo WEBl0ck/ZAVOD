1a6b97028f96e17d419aa6c0051a1f9772e1efad
Саша Кошарюк <a.koshariuk@dao-group.org>
2022-05-06 00:45:32 +0300

    🚨 lint fix

--- src/components/MainCanvas/MainCanvas.tsx
+++ src/components/MainCanvas/MainCanvas.tsx
@@ -1,28 +1,28 @@
-import { useEffect } from 'react';
+import { useEffect } from 'react'
 
-import MainScene from '../../scenes/MainScene';
+import MainScene from '../../scenes/MainScene'
 
-import { config } from '../../configs/phaser.config';
+import { config } from '../../configs/phaser.config'
 
-import PixelCoinIcon from '../../../public/assets/images/pixel-coin.png';
+import PixelCoinIcon from '/assets/images/pixel-coin.png'
 
-import './mainCanvas.scss';
+import './mainCanvas.scss'
 
 function MainCanvas() {
   useEffect(() => {
     async function initPhaser() {
       // Need to initialize Phaser here otherwise Gatsby will try to SSR it
-      const Phaser = await import('phaser');
+      const Phaser = await import('phaser')
 
       const game = new Phaser.Game({
         ...config,
         parent: 'main',
         scene: [MainScene],
-      });
+      })
     }
 
-    initPhaser();
-  }, []);
+    initPhaser()
+  }, [])
 
   return (
     <>
@@ -32,7 +32,7 @@ function MainCanvas() {
         <img src={PixelCoinIcon} alt="pixel coin icon" />
       </div>
     </>
-  );
+  )
 }
 
-export default MainCanvas;
+export default MainCanvas
