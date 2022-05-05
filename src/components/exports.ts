5e18cf653db2e5d6c7d0ff68f03294da2afc225b
Саша Кошарюк <a.koshariuk@dao-group.org>
2022-05-05 20:35:30 +0300

    📦 update components exports

--- src/components/exports.ts
+++ src/components/exports.ts
@@ -1,2 +1,13 @@
-export { default as MainCanvas } from '../components/MainCanvas/MainCanvas';
-export { default as Sidebar } from '../components/Sidebar/Sidebar';
+// regular components
+export { default as MainCanvas } from '../components/MainCanvas/MainCanvas'
+export { default as Sidebar } from '../components/Sidebar/Sidebar'
+export { default as Tabs } from '../components/Tabs/Tabs'
+export { default as Tab } from '../components/Tabs/Tab/Tab'
+
+// tabs
+export { default as BuffsTab } from '../components/Tabs/Tab/BuffsTab'
+export { default as CraftTab } from '../components/Tabs/Tab/CraftTab'
+export { default as ShopTab } from '../components/Tabs/Tab/ShopTab'
+export { default as StatsTab } from '../components/Tabs/Tab/StatsTab'
+export { default as StorageTab } from '../components/Tabs/Tab/StorageTab'
+export { default as UpgradeTab } from '../components/Tabs/Tab/UpgradeTab'
