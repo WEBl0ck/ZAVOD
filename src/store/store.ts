692ce27d88434339cfe657dec45ac1f592708b8a
WEBl0ck <antontamazlukar@gmail.com>
2022-05-23 23:40:17 +0300

    🚨 updated store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,17 +1,26 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './slices/counterSlice/counterSlice'
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+
+import mainReducer from './slices/mainSlice/mainSlice'
 import inventorySlice from './slices/inventorySlice/inventorySlice'
 import itemsSlice from './slices/itemsSlice/itemsSlice'
 import resourcesSlice from './slices/resourcesSlice/resourcesSlice'
+import shopSlice from './slices/shopSlice/shopSlice'
+import upgradeSlice from './slices/upgradeSlice/upgradeSlice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    main: mainReducer,
     inventory: inventorySlice,
     items: itemsSlice,
     resources: resourcesSlice,
+    shop: shopSlice,
+    upgrades: upgradeSlice,
   },
 })
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
+
+export const useAppDispatch = () => useDispatch<AppDispatch>()
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
